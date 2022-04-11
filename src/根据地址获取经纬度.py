5968b97f766e2224c12c4e30e7047df3b44f96b4
西西嘛呦 <461600371@qq.com>
2022-04-11 10:46:47 +0800

    Create 根据地址获取经纬度.py

---
+++ src/根据地址获取经纬度.py
@@ -0,0 +1,55 @@
+import requests
+import re
+
+"""原始网址：https://scorpionfree98.github.io/fetch_address_detail/Geocoder.html"""
+headers = {
+    "Host": "restapi.amap.com",
+    "Connection": "keep-alive",
+    "sec-ch-ua": '"Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+    "sec-ch-ua-mobile": "?0",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36",
+    "sec-ch-ua-platform": "Windows",
+    "Accept": "*/*",
+    "Sec-Fetch-Site": "cross-site",
+    "Sec-Fetch-Mode": "no-cors",
+    "Sec-Fetch-Dest": "script",
+    "Referer": "https://scorpionfree98.github.io/",
+    "ccept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "zh-CN,zh;q=0.9",
+    "Cookie": "cna=sR3HGhMV7GICAXFu3D7nMono; xlly_s=1; isg=BLGxbeR8dwUgPtsRHIzedIPhwD1LniUQ4kH-_ZPGrXiXutEM2-414F_b2E7ccr1I",
+}
+
+
+def get_coordinates(keywords):
+    """用于获取地址的经纬度
+    这里的请求接口返回的是一个搜索排序结果，取第一个
+    """
+    url1 = "https://restapi.amap.com/v3/assistant/inputtips?" \
+           "s=rsv3&" \
+           "key=160cab8ad6c50752175d76e61ef92c50&" \
+           "callback=jsonp_131307_&" \
+           "platform=JS&logversion=2.0&" \
+           "appname=https%3A%2F%2Fscorpionfree98.github.io%2Ffetch_address_detail%2FGeocoder.html&" \
+           "csid=DFA41225-FA40-4D94-9E04-4EF449082F05&" \
+           "sdkversion=1.4.18&" \
+           "keywords={}".format(keywords)
+    response = requests.get(url=url1, headers=headers)
+    response_re = re.search("jsonp_[0-9]+_\((.*?)\)$", response.text)
+    res = {
+        "query_keywords": keywords,
+        "return_keywords": "",
+        "coordinates": "",
+    }
+
+    try:
+        data = eval(response_re.groups()[0])
+        data = data['tips']
+        if data:
+            data = data[0]
+            return_keywords = data['name']
+            coordinates = data['location']
+            res['return_keywords'] = return_keywords
+            res['coordinates'] = coordinates
+    except Exception as e:
+        print(e)
+    return res
